9612a79fae04c5008157b618576065a039305e2a
AnhDungg <dung182003@gmail.com>
2025-02-21 08:54:47 +0700

    Thêm code vào UserRepository

---
+++ LOMSAPI/Repositories/Users/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using LOMSAPI.Models;
+
+namespace LOMSAPI.Repositories.Users
+{
+    public interface IUserRepository
+    {
+        Task<string> Authencate(LoginRequest loginRequest);
+        Task<bool> RegisterAsync(RegisterModel model);
+        Task<bool> VerifyEmailAsync(VerifyOtpModel model);
+        Task<bool> RequestPasswordResetAsync(ForgotPasswordModel model);
+        Task<bool> ResetPasswordAsync(ResetPasswordModel model);
+    }
+}
