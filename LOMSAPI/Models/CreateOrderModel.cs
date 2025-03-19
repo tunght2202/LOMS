19d9426c0b711afc7124b9c83c3570646814762b
AnhDungg <dung182003@gmail.com>
2025-03-20 03:03:55 +0700

    Coding CreateOrder

---
+++ LOMSAPI/Models/CreateOrderModel.cs
@@ -0,0 +1,7 @@
+﻿namespace LOMSAPI.Models
+{
+    public class CreateOrderModel
+    {
+        public string Comment { get; set; }
+    }
+}
---
+++ LOMSAPI/Repositories/Orders/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using LOMSAPI.Data.Entities;
+
+namespace LOMSAPI.Repositories.Orders
+{
+    public interface IOrderRepository
+    {
+        Task<int> CreateOrder();
+    }
+}
