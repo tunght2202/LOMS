a5c38566b41033f9343a2c2fc14bf01a89a246a1
Luong1406 <luongnvhs163502@fpt.edu.vn>
2025-04-03 15:41:08 +0700

    fix livestreamcontroller

---
+++ LOMSAPI/Controllers/RevenuesController.cs
@@ -0,0 +1,21 @@
+﻿using LOMSAPI.Repositories.Revenues;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LOMSAPI.Controllers
+{
+    public class RevenuesController : ControllerBase
+    {
+        private readonly IRevenueRepository _revenueRepository;
+
+        public RevenuesController(IRevenueRepository revenueRepository)
+        {
+            _revenueRepository = revenueRepository;
+        }
+
+        public async Task<decimal> GetTotalRevenue() => await _revenueRepository.GetTotalRevenue();
+
+        public async Task<int> GetTotalOrders() => await _revenueRepository.GetTotalOrders();
+
+        public async Task<decimal> GetRevenueByLivestreamId(int livestreamId) => await _revenueRepository.GetRevenueByLivestreamId(livestreamId);
+    }
+}
