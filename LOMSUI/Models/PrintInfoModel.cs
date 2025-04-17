50ebd914dd42be53a8ef1974f8fa4aed51028a29
tunght2202 <hathanhtung2202@gmail.com>
2025-04-17 12:55:43 +0700

    cma

---
+++ LOMSUI/Models/PrintInfoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOMSUI.Models
+{
+    public class PrintInfoModel
+    {
+        public string MaDonHang { get; set; }
+        public string TenKhachHang { get; set; }
+        public string MaVach { get; set; }
+        public DateTime NgayGio { get; set; }
+        public string DiaChi { get; set; }
+        public string SoDienThoai { get; set; }
+    }
+}
