d32800ee9654a3a8b9c7026292a4598606a8ecc0
AnhDungg <dung182003@gmail.com>
2025-04-22 14:38:27 +0700

    aaa

--- LOMSAPI/Models/RegisterRequestModel.cs
+++ LOMSAPI/Models/RegisterRequestModel.cs
@@ -12,13 +12,13 @@ public class RegisterRequestModel
 
     [Required, MinLength(6)]
     public string Password { get; set; }
-    [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại phải có 10 chữ số và bắt đầu bằng số 0.")]
+    [Required,RegularExpression(@"^0\d{9}$", ErrorMessage = "Phone number must be 10 digits and start with 0.")]
     public string PhoneNumber { get; set; }
     
     public string? FullName { get; set; }
     
     public string? Address { get; set; }
-    [RegularExpression("^(Male|Female)$", ErrorMessage = "Gender chỉ được là 'Male' hoặc 'Female'.")]
+    [RegularExpression("^(Male|Female)$", ErrorMessage = "Gender can only be 'Male' or 'Female'.")]
     public string? Gender { get; set; }
 
     
