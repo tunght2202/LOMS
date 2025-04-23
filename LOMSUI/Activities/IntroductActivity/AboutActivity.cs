f7217f464b00c1db814eefd6a03ee0350b5ad1d9
Pham Van Manh <121953255+ManhMau@users.noreply.github.com>
2025-04-23 13:29:01 +0700

    add

--- LOMSUI/Activities/IntroductActivity/AboutActivity.cs
+++ LOMSUI/Activities/IntroductActivity/AboutActivity.cs
@@ -1,6 +1,6 @@
 namespace LOMSUI.Activities;
 
-[Activity(Label = "AboutActivity")]
+[Activity(Label = "About Us")]
 public class AboutActivity : BaseActivity
 {
     protected override void OnCreate(Bundle? savedInstanceState)
