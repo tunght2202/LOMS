5912759df6cc2ef6ab4f9cc69036635ea0b84d60
Luong1406 <luongnvhs163502@fpt.edu.vn>
2025-05-08 13:00:56 +0700

    add auto get information of customer

---
+++ Information/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Information.Models;
+
+public partial class Comment
+{
+    public string CommentId { get; set; } = null!;
+
+    public string Content { get; set; } = null!;
+
+    public DateTime CommentTime { get; set; }
+
+    public int LiveStreamCustomerId { get; set; }
+
+    public virtual LiveStreamCustomer LiveStreamCustomer { get; set; } = null!;
+
+    public virtual Order? Order { get; set; }
+}
---
+++ Information/Models/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Information.Models;
+
+public partial class Role
+{
+    public string Id { get; set; } = null!;
+
+    public string? Name { get; set; }
+
+    public string? NormalizedName { get; set; }
+
+    public string? ConcurrencyStamp { get; set; }
+
+    public virtual ICollection<RoleClaim> RoleClaims { get; set; } = new List<RoleClaim>();
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
